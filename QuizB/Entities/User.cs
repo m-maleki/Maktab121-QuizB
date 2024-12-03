bd3ef61fb7508df2304f42e5dc1bb0dad726045e
safoura <safoora.ahmadiasl@gmail.com>
2024-12-03 23:24:53 +0330

    add emial to user class

--- QuizB/Entities/User.cs
+++ QuizB/Entities/User.cs
@@ -5,5 +5,6 @@ public class User
     public int Id { get; set; }
     public string FirstName { get; set; }
     public string LastName { get; set; }
+    public string email { get; set; }
     public List<Card> Cards { get; set; }
 }
