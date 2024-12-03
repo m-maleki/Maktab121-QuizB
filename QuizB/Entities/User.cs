6a16636c5967eeb3a2016d3301d3b0eac321c406
maleki <maleki@MASOUD>
2024-12-03 23:21:08 +0330

    add property phonenumber to entity user

--- QuizB/Entities/User.cs
+++ QuizB/Entities/User.cs
@@ -6,4 +6,5 @@ public class User
     public string FirstName { get; set; }
     public string LastName { get; set; }
     public List<Card> Cards { get; set; }
+    public string PhoneNumber { get; set; }
 }
